fcc5d1e2d9f38ba6ff764fd9c0e7f6d74a9b4e3c
Rohit Singh <rohit.singh@mobileprogramming.com>
2023-03-25 17:32:13 +0530

    Updated Code Flow

--- src/components/Loaders.jsx
+++ src/components/Loaders.jsx
@@ -1,43 +1,19 @@
 import React from 'react'
 import ReactDOM from "react-dom";
+import Lottie from "lottie-react";
+import Loader from "assets/loader/loader.json"
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const loaderContainer = document.getElementById("loader");
 
-const Loader = ({modalHandler}) => {
+const Loader = React.memo(() => {
     return (
       <div className="modal" style={{display:"block"}}>
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">Modal title</h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-                onClick={() => modalHandler(false)}
-              />
-            </div>
-            <div className="modal-body">
-              <p>Modal body text goes here.</p>
-            </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-bs-dismiss="modal"
-                onClick={() => modalHandler(false)}
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        </div>
+        <Lottie animationData={Loader} loop={true} />
       </div>
     );
-}
+})
 
-export const LoaderComponent = ({modalHandler}) => {
-  return ReactDOM.createPortal(<Loader modalHandler={modalHandler}/>,loaderContainer)
-}
+export const LoaderComponent = React.memo(() => {
+    return ReactDOM.createPortal(<Loader/>,loaderContainer)
+})
