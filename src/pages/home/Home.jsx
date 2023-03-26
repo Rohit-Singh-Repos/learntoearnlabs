89965c47b5fb3cc285131949c7db9c9290c449d7
Rohit Singh <rohit.singh@mobileprogramming.com>
2023-03-26 08:35:59 +0530

    Updated Code Flow

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 
 export const HomePage = React.memo(() => {
   return (
-    <div>HomePage</div>
+    <div className='container-fluid'>
+      <div className='row align-items-center p-3'>
+        <div className='col-sm-12 col-md-7 col-lg-8'>HomePage</div>
+        <div className='col-sm-12 col-md-5 col-lg-4'>HomePage</div>
+        <div class="text-bg-primary border-light mt-5 d-flex p-4">
+          <div class="card col-sm-12 col-md-6 col-lg-3 bg-transparent border-0 border-end rounded-0 border-4">
+            <div className='card-body'>
+              Hello
+            </div>
+          </div>
+          <div class="card col-sm-12 col-md-6 col-lg-3 bg-transparent border-0 border-end rounded-0 border-4">
+            <div className='card-body'>
+              Hello
+            </div>
+          </div>
+          <div class="card col-sm-12 col-md-6 col-lg-3 bg-transparent border-0 border-end rounded-0 border-4">
+            <div className='card-body'>
+              Hello
+            </div>
+          </div>
+          <div class="card col-sm-12 col-md-6 col-lg-3 bg-transparent border-0">
+            <div className='card-body'>
+              Hello
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 })
