64cffa63736bf29de11a6bc798a9c09c4254d0e0
Rohit-Singh-Repos <errohitsingh3010@gmail.com>
2023-06-19 01:16:10 +0530

    Updated Code with valid email address

--- src/pages/contact/ContactComponent2.jsx
+++ src/pages/contact/ContactComponent2.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 
 export const ContactFormAreaComponent = React.memo(({inputSchemas}) => {
     const [showAlertDanger, setShowAlertDanger] = useState(false);
+    const [showAlertDanger2, setShowAlertDanger2] = useState(false);
     const [showAlertSuccess, setShowAlertSuccess] = useState(false);
     const [showAlertNetwork, setShowAlertNetwork] = useState(false);
     const [inputVal, setInputVal] = useState({
@@ -30,39 +31,50 @@ export const ContactFormAreaComponent = React.memo(({inputSchemas}) => {
     const submitContactInfo = async() => {
       const { studentName, studentEmail, studentMobile, studentMessage, studentProfession } = inputVal
       if(studentName && studentEmail && studentMobile && studentMessage && studentProfession){
-        try {
-          const response = await axios(`${process.env.REACT_APP_API_BASE_URL}/api/contacts/`,{
-            method:"POST",
-            data:{
-              name:studentName,
-              profession:studentProfession,
-              email:studentEmail,
-              number:studentMobile,
-              msgbody:studentMessage,
-              datetime:new Date().toLocaleString(),
-            },
-          })
-          setShowAlertDanger(false);
-          setShowAlertNetwork(false);
-          if(response.status === 200){
-            setShowAlertSuccess(true);
-            setInputVal({
-              studentName:"",
-              studentEmail:"",
-              studentMobile:"",
-              studentMessage:"",
-              studentProfession:""
+        const emailRegExp = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+        if(studentEmail.match(emailRegExp)){
+          try {
+            const response = await axios(`${process.env.REACT_APP_API_BASE_URL}/api/contacts/`,{
+              method:"POST",
+              data:{
+                name:studentName,
+                profession:studentProfession,
+                email:studentEmail,
+                number:studentMobile,
+                msgbody:studentMessage,
+                datetime:new Date().toLocaleString(),
+              },
             })
+            setShowAlertDanger(false);
+            setShowAlertNetwork(false);
+            setShowAlertDanger2(false)
+            if(response.status === 200){
+              setShowAlertSuccess(true);
+              setInputVal({
+                studentName:"",
+                studentEmail:"",
+                studentMobile:"",
+                studentMessage:"",
+                studentProfession:""
+              })
+            }
+          } catch (error) {
+            setShowAlertNetwork(true)
+            setShowAlertSuccess(false);
+            setShowAlertDanger(false);
+            setShowAlertDanger2(false)
           }
-        } catch (error) {
-          setShowAlertNetwork(true)
+        }else{
+          setShowAlertDanger2(true)
           setShowAlertSuccess(false);
           setShowAlertDanger(false);
+          setShowAlertNetwork(false)
         }
       }else{
         setShowAlertDanger(true);
         setShowAlertSuccess(false);
         setShowAlertNetwork(false)
+        setShowAlertDanger2(false)
       }
     }
     return (
@@ -72,6 +84,7 @@ export const ContactFormAreaComponent = React.memo(({inputSchemas}) => {
                 Submit your query
               </SubHeading>
               {showAlertDanger && <Alert alertMessage="All fields are required" alertType="alert-danger fw-bold" setShowAlert={setShowAlertDanger}/>}
+              {showAlertDanger2 && <Alert alertMessage="Enter valid email address" alertType="alert-danger fw-bold" setShowAlert={setShowAlertDanger2}/>}
               {showAlertSuccess && <Alert alertMessage="Query Successfully Submitted" alertType="alert-success fw-bold" setShowAlert={setShowAlertSuccess}/>}
               {showAlertNetwork && <Alert alertMessage="Something Went Wrong" alertType="alert-danger fw-bold" setShowAlert={setShowAlertNetwork}/>}
               {textInput && textInput.length !== 0 ? (
